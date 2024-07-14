be89512636291010699ed3997b72677487a03d2c
avigail1 <a4182576@gmail.com>
2024-07-14 11:21:31 +0300

    Apdate HTML and JS files

--- script.js
+++ script.js
@@ -11,4 +11,17 @@ document.getElementById('clearCookiesButton').addEventListener('click', function
     }
     
     alert("העוגיות נמחקו בהצלחה!");
+});document.getElementById('clearCookiesButton').addEventListener('click', function() {
+    // קבלת כל העוגיות של הדומיין הנוכחי
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        // ניקוי העוגיה ע"י הגדרה שלה לפגת תוקף בעבר
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+    
+    alert("העוגיות נמחקו בהצלחה!");
 });
